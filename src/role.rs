f9ebbff66725393d1f6ded7a60866caf63dd2437
Scott Donnelly <scott@donnel.ly>
2022-07-02 21:03:16 +0100

    feat: derive Serializable for Claims and implement Dispaly and from_str for Role

--- src/role.rs
+++ src/role.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 use dynomite::Attribute;
 use serde::{Deserialize, Serialize};
 
@@ -6,4 +7,22 @@ use serde::{Deserialize, Serialize};
 pub enum Role {
     User,
     Admin,
+}
+
+impl Role {
+    pub fn from_str(role: &str) -> Role {
+        match role {
+            "Admin" => Role::Admin,
+            _ => Role::User,
+        }
+    }
+}
+
+impl fmt::Display for Role {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Role::User => write!(f, "User"),
+            Role::Admin => write!(f, "Admin"),
+        }
+    }
 }
